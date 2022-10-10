2059b088807658159871aaf80ad63c616d7ea6f0
esteri1 <eadeyemi24@gmail.com>
2022-10-10 04:51:53 -0700

    lists.h

---
+++ 0x05-python-exceptions/lists.h
@@ -0,0 +1,31 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+def magic_calculation(a, b):
+void print_python_list(PyObject *p);
+
+#endif /* LISTS_H */
